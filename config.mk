618c4cf2cb8250611c68be0a1b400602a50409c9
Jean Cochrane <jean@jeancochrane.com>
2017-11-16 09:45:03 -0600

    Scrape data for October 2017

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ SHELL := bash
 
 # Project-specific variables
 year = 2017
-month = 08
+month = 10
 
 tabula-download = "https://github.com/tabulapdf/tabula-java/releases/download/0.9.2/tabula-0.9.2-jar-with-dependencies.jar"
 tabula = java -jar tabula-java/tabula-0.9.2-jar-with-dependencies.jar
